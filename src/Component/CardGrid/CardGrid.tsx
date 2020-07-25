3731b70854a936de41ee5b591f2d58cc4eb653c1
Wayne Yao <wayneyao99@gmail.com>
2020-07-25 22:18:43 +1200

    fixed API key

--- src/Component/CardGrid/CardGrid.tsx
+++ src/Component/CardGrid/CardGrid.tsx
@@ -19,7 +19,7 @@ function CardGrid(props: ICardGridProps){
 
 
     useEffect(() => {
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=' + props.SearchQuery + '&appid=' + process.env.REACT_APP_API_KEY)
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=' + props.SearchQuery + '&appid=' + '23fce8b17a0f886b6185b21d15b80091')
             .then(response => response.json())
             .then(response => {
                 setItem({icon: response.weather[0].icon, main: response.weather[0].main, description: response.weather[0].description, temp: response.main.temp})
